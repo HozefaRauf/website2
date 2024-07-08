87ae25c75af350dc241f230303ad531b025197eb
HozefaRauf <muhammadhozaifa24@gmail.com>
2024-07-08 14:58:13 +0500

    first

---
+++ website/src/components/Container.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../css/components.css'
+
+function Container() {
+  return (
+    <div>
+        <input className='inputField'/>
+        
+    </div>
+  )
+}
+
+export default Container
---
+++ website/src/js/LoginSignup.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Container from '../components/Container.js'
+
+export default function LoginSignup() {
+  return (
+    <div>
+        <Container />
+        <p>hhhhhhhhh</p>
+    </div>
+    
+  )
+}

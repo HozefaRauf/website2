da5401bd06bdd0ac338b9b3b9564d042d92e4368
HozefaRauf <muhammadhozaifa24@gmail.com>
2024-07-09 14:31:14 +0500

    new commit office

--- website/src/js/LoginSignup.js
+++ website/src/js/LoginSignup.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Container from '../components/Container.js'
+import '../css/LoginSignup.css'
 
 export default function LoginSignup() {
   return (
-    <div>
-        <Container />
-        <p>hhhhhhhhh</p>
+    <div className='login-app'>
+        <div className='loginContainer'>
+          <Container />
+
+        </div>
     </div>
     
   )

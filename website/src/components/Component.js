7acbd99c51537bc35f95cb8a34007125cb42061a
HozefaRauf <muhammadhozaifa24@gmail.com>
2024-07-10 11:21:01 +0500

    3/ 10 july

--- website/src/components/Component.js
+++ website/src/components/Component.js
@@ -3,6 +3,14 @@ import styled from 'styled-components';
 export const Input=styled.input`
 background-color: #eee;
 border: 1px solid #DCDCDC;
+width: 100%;
+padding: 1.3% 1.8%;
+border-radius: 1.8%/2.5%;
+box-sizing: border-box;
+margin: 1% 0%;
 
+`;
+
+export const Button=styled.button`
 
 `;

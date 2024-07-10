c0808db25dad5bb4ab4794c3c0a8a5e16752167c
HozefaRauf <muhammadhozaifa24@gmail.com>
2024-07-10 15:46:13 +0500

    day completed

--- website/src/js/LoginSignup.js
+++ website/src/js/LoginSignup.js
@@ -6,10 +6,13 @@ export default function LoginSignup() {
   return (
     <div className='login-app'>
         <div className='loginContainer'>
-          <form>
+          <form className='login-form'>
             <Component.Input type='email' placeholder='Email/Username'/>
             <Component.Input type='password' placeholder='Password'/>
-            <p>Forgot Password?</p>
+            <div className='forgot-remember'>
+              <label><input type='checkbox' id='remember-me' name='remember-me'/>Remember Me</label>
+              <p className='forgot'>Forgot Password?</p>
+            </div>
             <Component.Button>SIGN IN</Component.Button>
           </form>
 

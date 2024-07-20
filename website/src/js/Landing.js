cabc255f58df1cf0cacc26887b535b0f67ac6e33
HozefaRauf <muhammadhozaifa24@gmail.com>
2024-07-20 21:24:50 +0500

    d

--- website/src/js/Landing.js
+++ website/src/js/Landing.js
@@ -7,6 +7,8 @@ function Landing() {
         <div className='container2'>
         <p>PARMCRDINS011</p>
         <p>hhhhhh</p>
+        <p>PARMCRDINS011</p>
+        <p>hhhhhh</p>
     </div>
     </div>
     </>

bad6d12bd8af5699e2cbece75105fcec1255c589
HozefaRauf <muhammadhozaifa24@gmail.com>
2024-07-21 23:54:50 +0500

    .

--- website/src/js/Landing.js
+++ website/src/js/Landing.js
@@ -3,17 +3,17 @@ import '../css/Landing.css'
 function Landing() {
   return (
     <>
-    <div className="container1">
-        <div className='container2'>
-        <p>PARMCRDINS011</p>
-        <p>hhhhhh</p>
-        <p>PARMCRDINS011</p>
-        <p>hhhhhh</p>
-    </div>
-    <div className='container2'>
-        <p>PARMCRDINS01</p>
-    </div>
-    </div>
+  <div className="container1">
+      <div className='container2'>
+      <p>PARMCRDINS011</p>
+      <p>hhhhhh</p>
+      <p>PARMCRDINS011</p>
+      <p>hhhhhh</p>
+  </div>
+  <div className='container2'>
+      <p>PARMCRDINS01</p>
+  </div>
+  </div>
     </>
   )
 }

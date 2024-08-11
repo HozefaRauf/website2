39ce8b05086365d54772d8a9e005168f5f0fae52
HozefaRauf <muhammadhozaifa24@gmail.com>
2024-08-12 04:06:08 +0500

    ,

--- website/src/index.js
+++ website/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Landing from './js/Landing.js'
+import Landing from './js/LoginSignup.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
